c03323bb3b525e9f415eb6e6728b741e06c021ea
Prabhakar Kumar <pk912873@gmail.com>
2025-05-01 12:47:56 +0530

    * pattern: Pyramid

---
+++ pattern/pattern14.py
@@ -0,0 +1,17 @@
+rows = int(input("Kitni rows chahiye hollow pyramid me? "))
+
+i = 1
+while i <= rows:
+    space = 0
+    while space < rows - i:
+        print(" ", end="")
+        space += 1
+    k = 0
+    while k < (2 * i - 1):
+        if k == 0 or k == 2 * i - 2 or i == rows:
+            print("*", end="")
+        else:
+            print(" ", end="")
+        k += 1
+    print()
+    i += 1
