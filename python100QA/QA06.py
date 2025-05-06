32d2d7da120e1d77688e119f45edebcd87d3cb2d
Prabhakar Kumar <pk912873@gmail.com>
2025-05-06 18:51:03 +0530

    Check the givenis  Number Positive Negative or Zero

---
+++ python100QA/QA06.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a Number: "))
+if num>0:
+    print(f"{num} is posative number")
+elif(num == 0):
+    print("Zero")
+elif num<0:
+    print(f"{num} is Negative num")
+else:
+    print("Enter a number")
