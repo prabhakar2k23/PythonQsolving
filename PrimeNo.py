7bfdd2c0139201ece1d05fa25679611144ee418b
Prabhakar Kumar <pk912873@gmail.com>
2025-05-07 20:01:17 +0530

    Print all prime numbers between 1 and 100

--- PrimeNo.py
+++ PrimeNo.py
@@ -1,10 +1,11 @@
-n=-17
-if(n <= 1):
-    for i in range(2,(n//2)+1):
-       if(n%i==0):
-           print(f"{n} is not a prime number")
-           break
-       else:
-           print("prine number")
+n = 17
+
+if n <= 1:
+    print(f"{n} is not a prime number")
+else:
+    for i in range(2, (n // 2) + 1):
+        if n % i == 0:
+            print(f"{n} is not a prime number")
+            break
     else:
-        print(f"{n} is not a prime number")
+        print(f"{n} is a prime number")
